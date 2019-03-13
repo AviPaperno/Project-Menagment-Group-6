84fbd4240f5cbf317fc0c9c508c0b8464a806a18
Snusmumrick <vpap1997@mail.ru>
2019-03-13 09:12:13 +0300

    Lab2

---
+++ Lab2/src/test/java/viktor/paperno/demo/LabApplicationTests.java
@@ -0,0 +1,16 @@
+package viktor.paperno.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LabApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
